704fad9df84ea1e4213e40e608afef76e7b386ae
Slavi Pantaleev <slavi@devture.com>
2022-02-27 11:16:33 +0200

    Upgrade shared-secret-authenticator to v2

--- etc/services/shared_secret_authenticator.py
+++ etc/services/shared_secret_authenticator.py
@@ -3,7 +3,7 @@
 # Shared Secret Authenticator module for Matrix Synapse
 # Copyright (C) 2018 Slavi Pantaleev
 #
-# http://devture.com/
+# https://devture.com/
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -18,53 +18,106 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+from typing import Awaitable, Callable, Optional, Tuple
 
-import logging
 import hashlib
 import hmac
-from twisted.internet import defer
+import logging
+
+import synapse
+from synapse import module_api
 
 logger = logging.getLogger(__name__)
 
-class SharedSecretAuthenticator(object):
+class SharedSecretAuthProvider:
+    def __init__(self, config: dict, api: module_api):
+        for k in ('shared_secret',):
+            if k not in config:
+                raise KeyError('Required `{0}` configuration key not found'.format(k))
 
-    def __init__(self, config, account_handler):
-        self.account_handler = account_handler
-        self.sharedSecret = config['sharedSecret']
+        m_login_password_support_enabled = bool(config['m_login_password_support_enabled']) if 'm_login_password_support_enabled' in config else False
+        com_devture_shared_secret_auth_support_enabled = bool(config['com_devture_shared_secret_auth_support_enabled']) if 'com_devture_shared_secret_auth_support_enabled' in config else True
 
-    @defer.inlineCallbacks
-    def check_password(self, user_id, password):
-        # The password is supposed to be an HMAC of the user id, keyed with the shared secret.
-        # It's not really a password in this case.
-        given_hmac = password.encode('utf-8')
+        self.api = api
+        self.shared_secret = config['shared_secret']
 
-        logger.info('Authenticating user: %s', user_id)
+        auth_checkers: Optional[Dict[Tuple[str, Tuple], CHECK_AUTH_CALLBACK]] = {}
+        if com_devture_shared_secret_auth_support_enabled:
+            auth_checkers[("com.devture.shared_secret_auth", ("token",))] = self.check_com_devture_shared_secret_auth
+        if m_login_password_support_enabled:
+            auth_checkers[("m.login.password", ("password",))] = self.check_m_login_password
 
-        h = hmac.new(self.sharedSecret.encode('utf-8'), user_id.encode('utf-8'), hashlib.sha512)
+        enabled_login_types = [k[0] for k in auth_checkers]
+
+        if len(enabled_login_types) == 0:
+            raise RuntimeError('At least one login type must be enabled')
+
+        logger.info('Enabled login types: %s', enabled_login_types)
+
+        api.register_password_auth_provider_callbacks(
+            auth_checkers=auth_checkers,
+        )
+
+    async def check_com_devture_shared_secret_auth(
+        self,
+        username: str,
+        login_type: str,
+        login_dict: "synapse.module_api.JsonDict",
+    ) -> Optional[
+        Tuple[
+            str,
+            Optional[Callable[["synapse.module_api.LoginResponse"], Awaitable[None]]],
+        ]
+    ]:
+        if login_type != "com.devture.shared_secret_auth":
+            return None
+        return await self._log_in_username_with_token("com.devture.shared_secret_auth", username, login_dict.get("token"))
+
+    async def check_m_login_password(
+        self,
+        username: str,
+        login_type: str,
+        login_dict: "synapse.module_api.JsonDict",
+    ) -> Optional[
+        Tuple[
+            str,
+            Optional[Callable[["synapse.module_api.LoginResponse"], Awaitable[None]]],
+        ]
+    ]:
+        if login_type != "m.login.password":
+            return None
+        return await self._log_in_username_with_token("m.login.password", username, login_dict.get("password"))
+
+    async def _log_in_username_with_token(
+        self,
+        login_type: str,
+        username: str,
+        token: str,
+    ) -> Optional[
+        Tuple[
+            str,
+            Optional[Callable[["synapse.module_api.LoginResponse"], Awaitable[None]]],
+        ]
+    ]:
+        logger.info('Authenticating user `%s` with login type `%s`', username, login_type)
+
+        full_user_id = self.api.get_qualified_user_id(username)
+
+        # The password (token) is supposed to be an HMAC of the full user id, keyed with the shared secret.
+        given_hmac = token.encode('utf-8')
+
+        h = hmac.new(self.shared_secret.encode('utf-8'), full_user_id.encode('utf-8'), hashlib.sha512)
         computed_hmac = h.hexdigest().encode('utf-8')
 
-        try:
-            is_identical = hmac.compare_digest(computed_hmac, given_hmac)
-        except AttributeError:
-            # `hmac.compare_digest` is only available on Python >= 2.7.7
-            # Fall back to being somewhat insecure on older versions.
-            is_identical = (computed_hmac == given_hmac)
+        if not hmac.compare_digest(computed_hmac, given_hmac):
+            logger.info('Bad hmac value for user: %s', full_user_id)
+            return None
 
-        if not is_identical:
-            logger.info('Bad hmac value for user: %s', user_id)
-            defer.returnValue(False)
-            return
+        user_info = await self.api.get_userinfo_by_id(full_user_id)
+        if user_info is None:
+            logger.info('Refusing to authenticate missing user: %s', full_user_id)
+            return None
 
-        if not (yield self.account_handler.check_user_exists(user_id)):
-            logger.info('Refusing to authenticate missing user: %s', user_id)
-            defer.returnValue(False)
-            return
+        logger.info('Authenticated user: %s', full_user_id)
 
-        logger.info('Authenticated user: %s', user_id)
-        defer.returnValue(True)
-
-    @staticmethod
-    def parse_config(config):
-        if 'sharedSecret' not in config:
-            raise Exception('Missing sharedSecret parameter for SharedSecretAuthenticator')
-        return config
+        return full_user_id, None
